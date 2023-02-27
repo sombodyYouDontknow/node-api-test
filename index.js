6f676646bbe3e24bbf051ec34db57ebcc30e9da5
sachin <somebodyoudontknow77@gmail.com>
2023-02-27 15:41:17 +0100

    mongodb configured

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/hi', (req, res) => {
-  res.send({
-    message:"hi",
-    respon:"hello"
-  })
-})
+app.get("/about", (req, res) => {
+  res.send("what you want to know , how are you ");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sachinchandran2784:DsFrZL9uO2WF94bZ@cluster0.xml1fgj.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected!");
+  }).catch((error)=>{
+     console.log(error);
+  });
